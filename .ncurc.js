75cdde4628f7fa0b77c8b5bd66e8e1abeb2a2770
Matthew Holder <sixxgate@hotmail.com>
2021-01-07 19:19:33 -0600

    Updates and block updates for some packages

---
+++ .ncurc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    reject: [
+        // Many breakings changes in 0.9.
+        "buefy",
+        // Incompatible with serialport right now.
+        "electron",
+        // Still a little too beta for me.
+        "laravel-mix",
+    ],
+};
